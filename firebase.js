6006cb7eaab5bf5d40fbbdd72abeebf90f733476
nagylilbidaa <naglylilbidaa@gmail.com>
2026-09-24 21:48:38 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,16 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCjmj1TjIjWrJjhisP-Or8OFYJ3TCZRSxs",
-  authDomain: "nagli-6f321.firebaseapp.com",
-  projectId: "nagli-6f321",
-  storageBucket: "nagli-6f321.firebasestorage.app",
-  messagingSenderId: "19948171002",
-  appId: "1:19948171002:web:568a05fbdaba240879a343",
-  measurementId: "G-D96CXV6Z01"
+  apiKey: "ضع_هنا_apiKey",
+  authDomain: "ضع_هنا_authDomain",
+  projectId: "ضع_هنا_projectId",
+  storageBucket: "ضع_هنا_storageBucket",
+  messagingSenderId: "ضع_هنا_messagingSenderId",
+  appId: "ضع_هنا_appId"
 };
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
