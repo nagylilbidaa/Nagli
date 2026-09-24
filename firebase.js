1ecfa21b942898056d6ea7bcb6a09f243873afe9
nagylilbidaa <naglylilbidaa@gmail.com>
2026-09-24 22:15:48 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,14 +3,15 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "ضع_هنا_apiKey",
-  authDomain: "ضع_هنا_authDomain",
-  projectId: "ضع_هنا_projectId",
-  storageBucket: "ضع_هنا_storageBucket",
-  messagingSenderId: "ضع_هنا_messagingSenderId",
-  appId: "ضع_هنا_appId"
+  apiKey: "AIzaSyCjmj1TjIjWrJjhisP-Or8OFYJ3TCZRSxs",
+  authDomain: "nagli-6f321.firebaseapp.com",
+  databaseURL: "https://nagli-6f321-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "nagli-6f321",
+  storageBucket: "nagli-6f321.firebasestorage.app",
+  messagingSenderId: "19948171002",
+  appId: "1:19948171002:web:568a05fbdaba240879a343",
+  measurementId: "G-D96CXV6Z01"
 };
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
